6ac05ac38652c1b0bb1576f4472f53106fc40baf
Jason Hall <jasonahall1994@gmail.com>
2019-11-14 12:24:40 +0000

    initial commit

---
+++ LearningConcurrent/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace LearningConcurrent
+{
+    class Program
+    {
+        static ConcurrentDictionary<int, int> item = new ConcurrentDictionary<int, int>();
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(new ThreadStart(AddItem));
+            Thread thread2 = new Thread(new ThreadStart(AddItem));
+
+            thread1.Start();
+            thread2.Start();
+        }
+
+        static void AddItem() 
+        {
+            item.TryAdd(1, 2);
+            Console.WriteLine(item.Count);
+        }
+    }
+}
